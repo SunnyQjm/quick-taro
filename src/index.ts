b66cd98128a9738156133e47758a4923c5c9d6d1
SunnyQjm <qjm3662@mail.dlut.edu.cn>
2019-07-22 23:10:27 +0800

    添加QuickTaroMask、QuickTaroFloatBtn、QuickTaroFloatBtnMenu

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 export {default as QuickTaroBaseComponent} from './components/quick-taro-base-component';
 export {default as QuickTaroNavBar} from './components/quick-taro-nav-bar';
-export {default as QQuickTaroFloatBtn} from './components/quick-taro-float-btn';
+export {default as QuickTaroFloatBtn} from './components/quick-taro-float-btn';
+export {default as QuickTaroFloatBtnMenu} from './components/quick-taro-float-btn-menu';
+export {default as QuickTaroMask} from './components/quick-taro-mask';
